8c32a362daed5a91b86a8cd013ffac1414cdf528
Leandro Costa <leandrohernandes95@gmail.com>
2023-10-12 23:38:22 -0300

    feat: add favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,13 +13,9 @@ export default function Home() {
                 <span className="px-1 rounded-sm bg-fuchsia-800">VIP</span>
               </h1>
               <nav className="flex gap-5">
-                <a href="#" className="">
+                <a href="#" className="hover:underline underline-offset-2">
                   {" "}
                   All the finds
-                </a>
-                <a href="#" className="">
-                  {" "}
-                  Contact
                 </a>
               </nav>
             </div>
@@ -29,7 +25,7 @@ export default function Home() {
       <div className="bg-gray-900 text-white">
         <div className="mx-auto py-8 max-w-7xl flex flex-col gap-7 md:flex-row items-center justify-between px-4 sm:px-6 lg:px-8">
           <div>
-            <h2 className="text-3xl md:text-4xl flex-auto max-w-md leading-tight">
+            <h2 className="text-3xl md:text-5xl flex-auto max-w-lg md:leading-tight">
               The Best Hand-Picked Deals from{" "}
               <span
                 style={{
@@ -63,7 +59,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="">
+      <div className="pb-28">
         <div className="mx-auto py-8 max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 w-full">
             {Array.from({ length: 12 }, (_, k) => k + 1).map((_, index) => (
@@ -72,6 +68,12 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      <footer className="bg-gray-900">
+        <div className="mx-auto py-5 max-w-7xl px-4 sm:px-6 lg:px-8 text-white text-center md:text-right text-md">
+          <span>Copyright © {new Date().getFullYear()} bestfinds.vip</span>
+        </div>
+      </footer>
     </main>
   );
 }
